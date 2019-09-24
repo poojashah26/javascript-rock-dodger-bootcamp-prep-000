51f28dd51cc06c1e639e864525d19e79896f3223
Pooja Shah <pooja_shah26@hotmail.co.uk>
2019-09-24 13:22:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ function checkCollision(rock) {
                *    and the rock's right edge is < the DODGER's right edge;
                * 3. The rock's left edge is < the DODGER's right edge,
                *    and the rock's right edge is > the DODGER's right edge
-               */) 
+               */ 
 
 
 function createRock(x) {
