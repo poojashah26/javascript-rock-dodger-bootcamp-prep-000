35e603f3120c5409bb8d13f907b40e3ae239fb8c
Pooja Shah <pooja_shah26@hotmail.co.uk>
2019-09-24 12:35:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,7 +48,6 @@ function checkCollision(rock) {
                * 3. The rock's left edge is < the DODGER's right edge,
                *    and the rock's right edge is > the DODGER's right edge
                */) {
-  if ((rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) || (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) || (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge)) {
       return true
     }
   }
